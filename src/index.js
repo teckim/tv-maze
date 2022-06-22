a02bfc4deff1451ac4ba2161be2b24faecde3c05
Rich <adukyerer@gmail.com>
2022-06-22 11:09:18 +0000

    update index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import ShowList from './components/showList.js';
-import Popup from './components/popup.js';
-import renderpopup from './components/renderpopup.js';
-import showsApi from './api/shows.js';
+
 // Styles
 import './assets/css/reset.css';
 import './assets/css/layout.css';
@@ -12,17 +10,8 @@ import './assets/css/components/navbar.css';
 import './assets/css/components/footer.css';
 import './assets/css/components/show-list.css';
 import './assets/css/components/show-card.css';
-
+import './assets/css/components/popup.css';
 // Images
 import './assets/images/logo.png';
- 
+
 const showList = new ShowList();
-const showbtn = document.querySelectorAll('.btn');
-showbtn.forEach(btn => {  
-  btn.addEventListener('click', async (e) => {
-  const shows = await showsApi.getByPage(1);
-  let thisshows = shows.slice(0, 21);
-       console.log(thisshows)
-    renderpopup(thisshows, e.target.dataset.id);
-  });
-});
