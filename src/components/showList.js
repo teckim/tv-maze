1cee4eacd9eda851c7b73b447382bb149eb68d26
BHDHakim <hakim.bhd@gmail.com>
2022-06-22 13:02:37 +0300

    Display likes

--- src/components/showList.js
+++ src/components/showList.js
@@ -1,4 +1,5 @@
 import showsApi from '../api/shows.js';
+import likesApi from '../api/likes.js';
 import ShowCard from './showCard.js';
 
 export default class ShowList {
@@ -8,11 +9,20 @@ export default class ShowList {
 
   async fetchShows() {
     try {
+      const likes = await likesApi.getAll();
       const shows = await showsApi.getByPage(1);
 
-      this.shows = shows.slice(0, 21);
+      this.shows = shows
+        .slice(0, 21)
+        .map((show) => {
+          const { likes: likesCount } = likes
+            .find((like) => like.item_id === show.id) || { likes: 0 };
+
+          return { ...show, likes: likesCount };
+        });
+
       this.shows.forEach((show) => {
-        const showCard = new ShowCard({ ...show, likes: 2 });
+        const showCard = new ShowCard(show);
       });
     } catch (e) {
       console.error(e);
