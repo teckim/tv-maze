1f9d7ca4d324d8a37731cbf8659c93cc7237fb3f
Rich <adukyerer@gmail.com>
2022-06-23 17:52:06 +0000

    add comments mock test

---
+++ src/__mocks__/comments.js
@@ -0,0 +1,33 @@
+const comment1 = [
+  {
+    creation_date: '2022-06-22',
+    username: 'i love',
+    comment: 'it is',
+  },
+  {
+    creation_date: '2022-06-22',
+    comment: 'ssss',
+    username: 'ddd',
+  },
+  {
+    comment: 'liked',
+    username: 'rich',
+    creation_date: '2022-06-22',
+  },
+];
+
+const comment2 = [
+
+  {
+    creation_date: '2022-06-22',
+    username: 'kell',
+    comment: 'it is',
+  },
+  {
+    creation_date: '2022-06-22',
+    comment: 'ssss',
+    username: 'ddd',
+  },
+];
+
+export {comment2,comment1};
