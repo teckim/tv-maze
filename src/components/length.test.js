fe0834776dbda0139db6f2ae41d1df4ca56cf69b
Rich <adukyerer@gmail.com>
2022-06-23 17:54:30 +0000

    remove this lenght test

--- src/components/length.test.js
+++
@@ -1,8 +0,0 @@
-const findLengthOnly = require('./rendercomments.js');
-
-describe('Remove and add Task', () => {
-  test('add a task', async () => {
-    const r = await findLengthOnly(250);
-    expect(r).toBe(10);
-  });
-});
