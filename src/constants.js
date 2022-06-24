3768ed13c3adf67045d11516642977f3bef34a18
BHDHakim <hakim.bhd@gmail.com>
2022-06-24 14:17:03 +0300

    Add dist folder

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://api.tvmaze.com';
-export const API_INVOLVE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
-export const APP_ID = 'c1niSuEjR7oYxbLbJamC';
